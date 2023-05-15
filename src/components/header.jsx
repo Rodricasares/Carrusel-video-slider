14ec91d2f73440ad15d4839e1ab0542ac445d40d
Rodrigo <rodrigocantudo@gmail.com>
2023-05-15 19:26:20 +0200

    header.jsx

---
+++ src/components/header.jsx
@@ -0,0 +1,26 @@
+
+//import './header.css'
+
+function Header() {
+
+
+  return (
+    <>
+    <header>
+      <a href="#" className='brand'>Travel</a>
+      <div className='navigation'>
+        <div className='navigation-items'>
+          <a href='#'>Home</a>
+          <a href='#'>About</a>
+          <a href='#'>Explore</a>
+          <a href='#'>Gallery</a>
+          <a href='#'>Contact</a>
+          
+        </div>
+      </div>
+    </header>
+    </>
+  )
+}
+
+export default Header
